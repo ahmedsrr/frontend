108a1a1dd3364cb54c04632933fb4625b57bcb07
ahmedsarr <mohamedsarr97@gmail.com>
2025-11-26 07:37:00 +0000

    front end

---
+++ eventify-frontend/tailwind.config.cjs
@@ -0,0 +1,13 @@
+// eventify-frontend/tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = { // <-- Utiliser module.exports
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ eventify-frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default defineConfig({
+  plugins: [react()],
+  
+  // 👇 Configuration du Serveur (Port 3000)
+  server: {
+    port: 3000, 
+    host: '0.0.0.0', 
+  },
+  
+});
